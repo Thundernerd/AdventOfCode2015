96f37a821c1829db4861c17e116fd5e05301ae14
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2019-12-01 16:48:03 +0100

    ADD Day One - Puzzle One and Puzzle Two projects

---
+++ .NET Core/Day One/Puzzle One/PuzzleSolver.cs
@@ -0,0 +1,13 @@
+namespace TNRD.AdventOfCode.DayOne.PuzzleOne
+{
+    public class PuzzleSolver : Foundation.PuzzleSolver
+    {
+        public PuzzleSolver(int day, string sessionCookie) : base(day, sessionCookie)
+        {
+        }
+
+        public override void Solve()
+        {
+        }
+    }
+}
---
+++ .NET Core/Day One/Puzzle Two/PuzzleSolver.cs
@@ -0,0 +1,13 @@
+namespace TNRD.AdventOfCode.DayOne.PuzzleTwo
+{
+    public class PuzzleSolver : Foundation.PuzzleSolver
+    {
+        public PuzzleSolver(int day, string sessionCookie) : base(day, sessionCookie)
+        {
+        }
+
+        public override void Solve()
+        {
+        }
+    }
+}
