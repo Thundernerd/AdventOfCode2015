c275b3b86b73c4bf018d237a5f49cbb080100487
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2019-12-01 16:58:48 +0100

    ADD Day One - Puzzle One implementation

--- .NET Core/Day One/Puzzle One/PuzzleSolver.cs
+++ .NET Core/Day One/Puzzle One/PuzzleSolver.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+
 namespace TNRD.AdventOfCode.DayOne.PuzzleOne
 {
     public class PuzzleSolver : Foundation.PuzzleSolver
@@ -8,6 +11,19 @@ namespace TNRD.AdventOfCode.DayOne.PuzzleOne
 
         public override void Solve()
         {
+            int sum = Input.Select(ConvertToInt).Sum();
+            Console.WriteLine($"Puzzle answer for day {Day} is {sum}");
+        }
+
+        private int ConvertToInt(char value)
+        {
+            if (value == '(')
+                return 1;
+
+            if (value == ')')
+                return -1;
+
+            return 0;
         }
     }
 }
